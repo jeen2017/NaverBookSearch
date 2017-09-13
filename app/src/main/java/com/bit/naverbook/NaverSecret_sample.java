0376f3246d5e87d41cd6765442b5674d894cf6da
jeen2017 <ahn_jin@naver.com>
2017-09-13 15:20:53 +0900

    2017-09-13-secret 변경

---
+++ app/src/main/java/com/bit/naverbook/NaverSecret_sample.java
@@ -0,0 +1,14 @@
+package com.bit.naverbook;
+
+/**
+ * Created by AJ on 2017-09-13.
+ *
+ * NAVER_CLIENT_ID와 NAVER_CLIENT_SECRET 항목을
+ * 본읜의 NAVER 개발 APP의 정보로 수정한후
+ * 파일이름을 NaverSecret 
+ */
+
+public class NaverSecret_sample {
+    public static final String NAVER_CLIENT_ID = "YOUR_NAVER_CLIENT_ID";
+    public static final String NAVER_CLIENT_SECRET = "YOUR_NAVER_CLIENT_SECRET";
+}
